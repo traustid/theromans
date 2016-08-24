aace782be6b4a5e0aa0055f3e44fb757b13b0b34
Trausti Thor Johannsson <traustitj@mac.com>
2016-08-24 21:10:24 +0200

    moved tests to its own file

--- romans.js
+++ romans.js
@@ -113,18 +113,4 @@ RomanNumber.prototype.toRoman = function() {
 	return reply;
 };
 
-
-var test = [null, '', 0, 1, 3, 4, 5, 'I', 'III', 'IIII', 'IV', 'V', 1968, '1473', 2999, 3000, 10000, 'CDXXIX', 'CD1X', 'error', 'MCDLXXXII', 'MCMLXXX', 'MMMMCMXCIX', 'MMMMDMXCIX'];
-
-test.map(function(value) {
-	try {
-	var r = new RomanNumber(value);	
-	} catch (e) {
-		console.log("Illegal value : ", value, e.message);
-	}
-	finally {
-		if (r) {
-			console.log("Value : ", r.toString() + " = " + r.toInt());
-		}
-	}
-})
+module.exports = RomanNumber;
