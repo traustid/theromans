2eebec431dd8fa1731462cea3dce0f770b89e919
Trausti Thor Johannsson <traustitj@mac.com>
2016-08-24 19:22:10 +0200

    ahh, one and two chars, need to think this. Not going to work

--- romans.js
+++ romans.js
@@ -16,6 +16,20 @@ var readline = require("readline");
  I 1
 */
 
+var fromRoman = function(astring) {
+	if (typeof(astring) !== "string") {
+		return undefined;
+	}
+	var work = astring.split("");
+	var romans = { "M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1};
+	var not_done = true;
+
+	while (not_done) {
+
+	}
+
+
+}
 
 var fromNumber = function(anumber) {
 	var reply = "";
