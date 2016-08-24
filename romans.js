3f25b117a2ba4142ebd84d81ad810724269dc2cd
Trausti Thor Johannsson <traustitj@mac.com>
2016-08-24 19:04:09 +0200

    adding 4 and a list of available numbers

--- romans.js
+++ romans.js
@@ -1,5 +1,22 @@
 var readline = require("readline");
 
+/*
+ M 1000
+ CM 900
+ D 500
+ CD 400
+ C 100
+ XC 90
+ L 50
+ XL 40
+ X 10
+ IX 9
+ V 5
+ IV 4
+ I 1
+*/
+
+
 var fromRoman = function(anumber) {
 	var reply = "";
 	anumber = parseInt(anumber);
@@ -7,6 +24,10 @@ var fromRoman = function(anumber) {
 	while (anumber >= 5) {
 		reply += "V";
 		anumber -= 5;
+	}
+	while (anumber >= 4) {
+		reply += "IV";
+		anumber -= 4;
 	}
 	while (anumber >= 1) {
 		reply += "I";
